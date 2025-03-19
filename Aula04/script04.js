77d90073436b1cd79641ba313861f24a85fff750
Amanda Lacerda <87672345+AMANDALACER@users.noreply.github.com>
2025-03-19 18:27:25 -0300

    Aula 05

--- Aula04/script04.js
+++ Aula04/script04.js
@@ -22,9 +22,7 @@ function adicionarTarefa() {
         //array
         tarefas.push(tarefa)
         //chamando a função
-        renderizarTarefas()
-
-        
+        renderizarTarefas() 
     }
 
     //limpa o input do usuário
@@ -43,13 +41,50 @@ function renderizarTarefas() {
 
     //for (iterador, condição, fequencia)
 
-    let i = 0
-    for(i; i < tarefas.length; i++) {
+    
+    for(let i = 0; i < tarefas.length; i++) {
         let novaTarefa = document.createElement("li")
         novaTarefa.textContent = tarefas[i]
+
+        let botaoRemover = document.createElement("button") //criando o botão
+        botaoRemover.className = "remover" // criando classe
+        botaoRemover.textContent = "Remover" //adicionando texto dentro do botão
+        botaoRemover.onclick = () => removerTarefa(i)
+
+        let botaoEditar = document.createElement("button")
+        botaoEditar.className = "editar" // criando classe
+        botaoEditar.textContent = "Editar" //adicionando texto dentro do botão
+        botaoEditar.onclick = () => editarTarefa(i)
+
+
+        novaTarefa.appendChild(botaoRemover)
+        novaTarefa.appendChild(botaoEditar)
         listaTarefas.appendChild(novaTarefa)
     }
     
 }
 
+function removerTarefa(i) {
+    tarefas.splice(i, 1)
+    renderizarTarefas()
+}
+
+function editarTarefa(i) {
+    let tarefaEditada = prompt("Edite a tarefa:")
+    if (tarefaEditada.trim() !== "") {
+        tarefas[i] = tarefaEditada
+        renderizarTarefas()
+    }
+    
+}
+
+function limparLista() {
+    tarefas.length = 0
+    renderizarTarefas()
+    mensagem.textContent = "Lista  de tarefas removida com sucesso"
+
+}
+
+
 //Aula 04 Arrays (lsitas), estruturas de repetição (for) e escopo
+// Aula 054 Métodos de array(deletando e editando itens em lista)
